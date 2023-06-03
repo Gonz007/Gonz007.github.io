e14f6616fcd4927576183bddcc21f7cf35557697
Juan David Ortiz <51717867+Gonz007@users.noreply.github.com>
2023-06-03 14:41:20 -0500

    f

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var modelViewer = document.querySelector('#cubo3D model-viewer');
-
-    modelViewer.addEventListener('load', function() {
-        var model = modelViewer.model;
-        model.setScale(2); // Ajusta el valor según tus necesidades
-    });
-});
