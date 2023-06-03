e512c7ef96fc476d780c204d68e27afa7424c1d6
Juan David Ortiz <51717867+Gonz007@users.noreply.github.com>
2023-06-03 14:22:28 -0500

    Integracion 3d funcional viewer

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
-// Configuraciones básicas de Three.js
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+document.addEventListener('DOMContentLoaded', function() {
+    var modelViewer = document.querySelector('#cubo3D model-viewer');
 
-var renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById('cubo3D').appendChild(renderer.domElement);
-
-// Creando el cargador FBX
-var loader = new THREE.FBXLoader();
-
-// Cargando el modelo FBX
-loader.load('cubo.fbx', function(object) {
-    scene.add(object);
+    modelViewer.addEventListener('load', function() {
+        var model = modelViewer.model;
+        model.setScale(2); // Ajusta el valor según tus necesidades
+    });
 });
-
-// Función de renderizado
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-}
-animate();
